c5ae0ca33edca226f8df78e6251bce31e31077e8
Catharina Fonseca <catharinafonseca22@gmail.com>
2026-05-11 22:19:46 -0300

    Adicionando dados

--- JoinClassGestaodeHorario.API/obj/Debug/net9.0/JoinClassGestaodeHorario.API.AssemblyInfo.cs
+++ JoinClassGestaodeHorario.API/obj/Debug/net9.0/JoinClassGestaodeHorario.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JoinClassGestaodeHorario.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94c74129f3af22ebfd7ba4b40e95634c61051f8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc392c7d4b243734085756804eeaf6b7d7e76dc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("JoinClassGestaodeHorario.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JoinClassGestaodeHorario.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
