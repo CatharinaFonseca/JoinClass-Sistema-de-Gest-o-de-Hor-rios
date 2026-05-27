b4a27f37b6963187af330c2c5cfac4d6e923c365
Catharina Fonseca <catharinafonseca22@gmail.com>
2026-05-26 23:07:33 -0300

    Iniciando Service

--- JoinClassGestaodeHorario.API/Services/GerarHorariosService.cs
+++ JoinClassGestaodeHorario.API/Services/GerarHorariosService.cs
@@ -2,11 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JoinClassGestaodeHorario.API.Dominio.Entidade;
 
 namespace JoinClassGestaodeHorario.API.Services
 {
-    public class GerarHorariosService
+    public class GerarHorariosService(List<Turma> turmas, List<Professor> professores)
     {
-        
+        private readonly List<DayOfWeek> diasDaSemana = new List<DayOfWeek>()
+        {
+            //DayOfWeek é um enum pronto do C# que representa os dias da semana
+            //Determina os dias da semana
+            DayOfWeek.Monday,
+            DayOfWeek.Tuesday,
+            DayOfWeek.Wednesday,
+            DayOfWeek.Thursday,
+            DayOfWeek.Friday
+        };
     }
 }
