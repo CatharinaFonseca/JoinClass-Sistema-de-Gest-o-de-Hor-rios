e5fb45614403e9987c5a0a28400167c1e3ba9d62
Catharina Fonseca <catharinafonseca22@gmail.com>
2026-06-05 18:27:08 -0300

    minhas alterações

--- JoinClassGestaodeHorario.API/obj/Debug/net9.0/JoinClassGestaodeHorario.API.AssemblyInfo.cs
+++ JoinClassGestaodeHorario.API/obj/Debug/net9.0/JoinClassGestaodeHorario.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JoinClassGestaodeHorario.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7a879a20fdf84ced82ed6dbd3b3f0dfefb9501e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+364a54cb6e177a6ff65521f74800bd2005151e78")]
 [assembly: System.Reflection.AssemblyProductAttribute("JoinClassGestaodeHorario.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JoinClassGestaodeHorario.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
